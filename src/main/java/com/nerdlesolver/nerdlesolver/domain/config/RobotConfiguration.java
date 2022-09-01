357df98160f0ab4b72171ab73456f3517d22c246
pac Pac <paco0161@gmail.com>
2022-09-01 09:39:38 +0800

    NS_Formating_Structure

---
+++ src/main/java/com/nerdlesolver/nerdlesolver/domain/config/RobotConfiguration.java
@@ -0,0 +1,11 @@
+package com.nerdlesolver.nerdlesolver.domain.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.awt.*;
+
+@Configuration
+public class RobotConfiguration {
+
+}
